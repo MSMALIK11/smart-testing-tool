c82ad4438b2e7030bc545dcd2cb12449afa43ff1
aadarsh <aadarsh@liberintechnologies.com>
2022-06-21 17:32:44 +0530

    add port-status

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -33,7 +33,267 @@ function Home() {
 
             <div className="port-status">
                 <div className="status">
-                    <p>32</p>
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status lockopen">
+                    <p id='lockernumber'>32</p>
+                    <p>Open</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status lockopen">
+                    <p id='lockernumber'>32</p>
+                    <p>Open</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status lockopen">
+                    <p id='lockernumber'>32</p>
+                    <p>Open</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status lockopen">
+                    <p id='lockernumber'>32</p>
+                    <p>open</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status lockopen">
+                    <p id='lockernumber'>32</p>
+                    <p>Open</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status lockopen">
+                    <p id='lockernumber'>32</p>
+                    <p>Open</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
+                    <p>close | Empty</p>
+                </div>
+                <div className="status">
+                    <p id='lockernumber'>32</p>
                     <p>close | Empty</p>
                 </div>
 
